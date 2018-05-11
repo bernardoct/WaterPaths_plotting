722838b67a51d9bd6c24c012dce88dbfcc8c9063
bernardo <bct52@cornell.edu>
2018-05-11 16:34:27 -0400

    pathway_3d partially implemented

--- plotting/pathways_plotting.py
+++ plotting/pathways_plotting.py
@@ -3,6 +3,7 @@ from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+
 def reorder_pathways(pathways_utility):
     score = []
 
@@ -13,7 +14,37 @@ def reorder_pathways(pathways_utility):
 
     return [pathways_utility[i] for i in ix]
 
-def create_fixed_length_array(weeks, length):
+
+def create_fixed_length_array(weeks_vector, infra_option_or_npv, length):
+    fixed_length_array = np.zeros(length)
+
+    for i in range(1, len(weeks_vector)):
+        fixed_length_array[weeks_vector[i]:weeks_vector[i-1]] = infra_option_or_npv[i - 1]
+
+    fixed_length_array[weeks_vector[-1]:-1] = infra_option_or_npv[-1]
+
+    return fixed_length_array
+
+
+def plot_construction_moment(pathways_utility):
+    x, y = [], []
+    for i in range(len(pathways_utility)):
+        x = np.hstack((x, pathways_utility[i][0]))
+        y = np.hstack((y, pathways_utility[i][1]))
+
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=100)
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+    plt.clf()
+    plt.imshow(heatmap.T, extent=extent, origin='lower')
+    plt.show()
+
+
+def plot_3d_pathway_plot(pathways_utility, nweeks):
+
+    x, y = np.meshgrid(range(len(pathways_utility)), range(nweeks))
+
+
 
 
 def plot_pathways_id(pathways_all_rdms, s, rdm, savefig_directory=''):
@@ -29,17 +60,8 @@ def plot_pathways_id(pathways_all_rdms, s, rdm, savefig_directory=''):
         pathways_list_utility.append(pathways_list)
 
     durham_pathways_reordered = reorder_pathways(pathways_list_utility[1])
-    x, y = [], []
-    for i in range(len(durham_pathways_reordered)):
-        x = np.hstack((x, durham_pathways_reordered[i][0]))
-        y = np.hstack((y, durham_pathways_reordered[i][1]))
 
-    heatmap, xedges, yedges = np.histogram2d(x, y, bins=100)
-    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-
-    plt.clf()
-    plt.imshow(heatmap.T, extent=extent, origin='lower')
-    plt.show()
+    # plot_construction_moment(durham_pathways_reordered)
 
     if savefig_directory == '':
         plt.show()
